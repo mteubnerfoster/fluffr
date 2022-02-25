f2ade6a094128bfb294f52b6da722ef3e706813e
bravotango <seattleriot@hotmail.com>
2022-02-24 19:15:44 -0800

    Added paragraph

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import { Profile } from './components/Profile';
 import Home from './components/Home';
-
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
@@ -17,12 +16,13 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="App">
+        <div className='App'>
           <h1>Fluffr - The Dating App for Pets!</h1>
+          <p>Find your furever friend</p>
           <NavBar />
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/profile" component={Profile} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/profile' component={Profile} />
           </Switch>
           <Footer />
         </div>
