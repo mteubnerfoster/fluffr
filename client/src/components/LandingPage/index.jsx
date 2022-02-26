60a0e507d2505587a563ed4874d6c892d0e8fa87
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-02-26 13:50:14 -0800

    added background video for landing page

--- client/src/components/LandingPage/index.jsx
+++ client/src/components/LandingPage/index.jsx
@@ -1,23 +1,42 @@
 import "./style.css";
 import { Link } from "react-router-dom";
 import logo from "../../data/fluffr-logo-circle.png";
+import heroVid from "../../data/dog-hero-video.mp4";
 
 const LandingPage = () => {
-
   return (
-    <section className="container d-flex flex-column justify-content-around landingPageContainer">
-      <div className="lpLogo">
-        <img src={logo} alt="fluffr logo" />
-      </div>
-      <div className="lpName">
-        <h1>Fluffr</h1>
-      </div>
-      <div className="lpStart">
-        <Link to="/home" className="getStarted">
-          <h2>Get Started!</h2>
-        </Link>
-      </div>
-    </section>
+    <>
+      <section className="container d-flex flex-column justify-content-around landingPageContainer">
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            position: "absolute",
+            width: "100%",
+            left: "50%",
+            top: "50%",
+            height: "100%",
+            objectFit: "cover",
+            transform: "translate(-50%, -50%",
+            zIndex: "-1",
+          }}
+        >
+          <source src={heroVid} type="video/mp4" />
+        </video>
+        <div className="lpLogo">
+          <img className="img-fluid" src={logo} alt="fluffr logo" />
+        </div>
+        <div className="lpName">
+          <h1>Fluffr</h1>
+        </div>
+        <div className="lpStart">
+          <Link to="/home" className="getStarted">
+            <h2>Get Started!</h2>
+          </Link>
+        </div>
+      </section>
+    </>
   );
 };
 
