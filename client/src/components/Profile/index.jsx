8aec5b654ca335b487f5f05acc69cdd398e7e0fb
cosdaman <cosdaguy@gmail.com>
2022-03-01 22:12:24 -0800

    data pulled from db

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
 import Header from "../Header";
+import Auth from "../../utils/auth";
+import { QUERY_ME } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
 
 export const Profile = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  let favedPets = data.me.favoritePets;
+  console.log(favedPets);
   return (
     <>
       <Header />
