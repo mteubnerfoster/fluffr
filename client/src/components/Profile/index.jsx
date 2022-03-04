2ace57122cc8956645ff8a6a8cc21db5395f4df2
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-03-03 19:57:58 -0800

    synced up data to profile component and cleaned up landing page

--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -7,40 +7,24 @@ import Photo from "../../data/fluffr-logo-heart-inverted-iii.png";
 import Auth from "../../utils/auth";
 import { QUERY_ME } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
-import { Redirect} from 'react-router-dom';
+import { Redirect } from "react-router-dom";
 
 export const Profile = () => {
+  let pet;
+
   const { loading, data } = useQuery(QUERY_ME);
   if (loading) {
     return <div>Loading...</div>;
   }
 
   if (Auth.loggedIn()) {
-      if (data.me) {
-        let favedPets = data.me.favoritePets;
-        console.log(favedPets);
-      }
-  }else{
+    if (data.me) {
+      pet = data.me.favoritePets;
+      console.log(pet);
+    }
+  } else {
     return <Redirect to="/login" />;
   }
-
-  const pets = [
-    {
-      name: "name 1",
-      details: "dsjfljslfk;jslfjk;lsaflkjs;lfj;lsf",
-      link: "http://wwww.adboptmeimcool.biz",
-    },
-    {
-      name: "name 2",
-      details: "dsjfljslfk;jslfjk;lsaflkjs;lfj;lsf",
-      link: "http://wwww.adboptmeimcool.biz",
-    },
-    {
-      name: "name 3",
-      details: "dsjfljslfk;jslfjk;lsaflkjs;lfj;lsf",
-      link: "http://wwww.adboptmeimcool.biz",
-    },
-  ];
 
   return (
     <>
@@ -48,21 +32,26 @@ export const Profile = () => {
       <NavBar />
       <div className="container">
         <div className="row d-flex justify-content-evenly">
-          {pets.map((pet, index) => {
+          {pet.map((pet, index) => {
             return (
               <div
                 className="card col-sm-12 col-md-3 col-lg-3 m-1 text-white bg-black bg-opacity-25"
                 key={index}
               >
-                <img
-                  src={Photo}
+                <img id="petPhoto"
+                  src={pet.photo}
                   className="card-img-top"
                   alt="animal up for adoption"
                 />
                 <div className="card-body">
                   <h5 className="card-title">{pet.name}</h5>
-                  <p className="card-text">{pet.details}</p>
-                  <a href={pet.link} className="btn btn-danger">
+                  <p className="card-text">{pet.breed}</p>
+                  <a
+                    href={pet.linkToPet}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="btn btn-danger"
+                  >
                     Adopt Me!
                   </a>
                 </div>
